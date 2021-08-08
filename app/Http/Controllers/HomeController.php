2985ae3e59a054bf12dd21572dc9a3f60c349af5
aqilat94 <aqilat94@gmail.com>
2021-08-08 22:58:54 +0800

    add function notify to auth user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Notifications\TrainingNotification;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        auth()->user()->notify(new TrainingNotification);
         return view('home');
     }
 }
